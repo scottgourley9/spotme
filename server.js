034a9c45bdbe1782723a67da4fe55b2c5aec68a5
Scott Gourley <scottgourley9@gmail.com>
2016-11-22 19:47:49 -0700

    g

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var bodyParser = require('body-parser');
 var massive = require('massive');
 var connectionString = "postgres://postgres:pass1234@localhost/spotme";
 // var connectionString = config.connectionString;
-var db = massive.connectSync({ db : "spotme"});
-var massiveInstance = massive.connectSync({connectionString : connectionString})
+//var db = massive.connectSync({ db : "spotme"});
+var db = massive.connectSync({connectionString : connectionString})
 var app = module.exports = express();
 
 app.set('db', db);
