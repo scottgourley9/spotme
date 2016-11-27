a6b01bed8624b7f23e7940eeda1b50c34caf3b78
Scott Gourley <scottgourley9@gmail.com>
2016-11-26 23:00:48 -0700

    d

--- public/js/controllers/dashboardCtrl.js
+++ public/js/controllers/dashboardCtrl.js
@@ -1,5 +1,8 @@
 angular.module('spotme').controller('dashboardCtrl', function($auth, $scope, $state, messageService, userService){
 
+$scope.menuHidden = true;
+$scope.dashMenu = [];
+
   var payload = () => {
       var payloadData = $auth.getPayload()
       userService.user = payloadData.sub
@@ -8,8 +11,21 @@ angular.module('spotme').controller('dashboardCtrl', function($auth, $scope, $st
 
 $scope.currentUser = userService.user
 
+$scope.hamClick = function(){
+  $scope.dashMenu.splice(0)
+  if($scope.menuHidden){
+    $scope.dashMenu.push('animated bounceInRight')
+    $scope.menuHidden = !$scope.menuHidden;
+  }
+  else {
+    $scope.dashMenu.push('animated bounceOutRight')
+    setTimeout(function(){
+      $scope.menuHidden = !$scope.menuHidden;
+    }, 500)
+  }
 
-  
+}
+
   $scope.logout = function(){
     $auth.logout()
     $state.go('welcome')
--- public/js/controllers/welcomeCtrl.js
+++ public/js/controllers/welcomeCtrl.js
@@ -1,4 +1,24 @@
 angular.module('spotme').controller('welcomeCtrl', function($scope, $state, messageService){
+$scope.hamMenuShowing = false;
+$scope.slideMenu = []
+  $scope.hamClick = function(){
+    $scope.slideMenu.splice(0)
+
+    if(!$scope.hamMenuShowing){
+      $scope.slideMenu.push('animated bounceInDown')
+      $scope.hamMenuShowing = !$scope.hamMenuShowing
+    }
+    else {
+      $scope.slideMenu.push('animated bounceOutUp')
+      setTimeout(function(){
+        $scope.hamMenuShowing = !$scope.hamMenuShowing
+      }, 500)
+
+      // $scope.slideMenu.splice(0)
+    }
+
+  }
+
 
   var TxtType = function(el, toRotate, period) {
           this.toRotate = toRotate;
