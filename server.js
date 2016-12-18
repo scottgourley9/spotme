e4c9500f2f92f423fae0235e89e0992ac507bb32
Scott Gourley <scottgourley9@gmail.com>
2016-12-18 12:15:56 -0700

    complaint

--- server.js
+++ server.js
@@ -470,7 +470,7 @@ app.put('/api/negativemessage/:id', function(req, res){
   })
 })
 
-app.put('/api/complaint/:id', function(req, res){
+app.post('/api/complaint/:id', function(req, res){
   db.complaint([req.params.id, req.body.complaint], function(err, success){
     if(err){
       res.status(500).json(err)
