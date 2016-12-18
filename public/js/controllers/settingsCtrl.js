867101a072db3cd5cbbfb13e6828333a5aa57350
Scott Gourley <scottgourley9@gmail.com>
2016-12-17 21:14:20 -0700

    lots

--- public/js/controllers/settingsCtrl.js
+++ public/js/controllers/settingsCtrl.js
@@ -1,3 +1,58 @@
-angular.module('spotme').controller('settingsCtrl', function($scope, $state, messageService){
+angular.module('spotme').controller('settingsCtrl', function($scope, $state, messageService, userService){
+
+  $scope.changePassword = function(){
+    swal({
+  title: "Enter current password",
+  type: "input",
+  showCancelButton: true,
+  closeOnConfirm: false,
+  animation: "slide-from-top",
+  inputPlaceholder: "Password"
+},
+function(inputValue){
+  if (inputValue === false) return false;
+
+  if (inputValue === "") {
+    swal.showInputError("Write your current password");
+    return false
+  }
+  userService.checkPass(inputValue).then(function(res){
+    if(res.data){
+
+
+  swal({
+  title: "Enter NEW password",
+  type: "input",
+  showCancelButton: true,
+  closeOnConfirm: false,
+  animation: "slide-from-top",
+  inputPlaceholder: "New Password"
+},
+function(inputValue2){
+  if (inputValue2 === false) return false;
+
+  if (inputValue2 === "") {
+    swal.showInputError("Write your NEW password");
+    return false
+  }
+  userService.changePassword(inputValue2).then(function(resp){
+    if(resp.status === 200){
+      swal("Success", "Your password has been changed!", "success");
+    }
+    else {
+      swal("Sorry", "There was an error", "error");
+    }
+  })
+
+});
+}
+else {
+  swal("Error", "Incorrect password", "error");
+}
+})
+});
+
+  }
+
 
 })
