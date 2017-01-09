2f6e51a9860166d61a1737a329d05ecf02649271
Scott Gourley <scottgourley9@gmail.com>
2017-01-08 18:54:39 -0700

    e

--- public/js/controllers/frontPageCtrl.js
+++ public/js/controllers/frontPageCtrl.js
@@ -1,10 +1,32 @@
-angular.module('spotme').controller('frontPageCtrl', function($document){
-  $(document).ready(function () {
-  function reorient(e) {
-    var portrait = (window.orientation % 180 == 0);
-    $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
+angular.module('spotme').controller('frontPageCtrl', function($document, $scope, $state){
+//   $(document).ready(function () {
+//   function reorient(e) {
+//     var portrait = (window.orientation % 180 == 0);
+//     $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
+//   }
+//   window.onorientationchange = reorient;
+//   window.setTimeout(reorient, 0);
+// });
+$scope.hamMenuShowing = false;
+$scope.slideMenu = []
+  $scope.hamClick = function(){
+    $scope.slideMenu.splice(0)
+
+    if(!$scope.hamMenuShowing){
+      $scope.slideMenu.push('animated bounceInDown')
+      $scope.hamMenuShowing = !$scope.hamMenuShowing
+    }
+    else {
+      $scope.slideMenu.push('animated bounceOutUp')
+      setTimeout(function(){
+        $scope.hamMenuShowing = !$scope.hamMenuShowing
+      }, 500)
+
+      // $scope.slideMenu.splice(0)
+    }
+
   }
-  window.onorientationchange = reorient;
-  window.setTimeout(reorient, 0);
-});
+  $scope.about = function(){
+    $state.go('welcome')
+  }
 })
